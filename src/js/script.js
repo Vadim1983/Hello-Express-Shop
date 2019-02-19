572ec04f48b94c03295e38e1da4b091b5687b98c
Вадим <vshhukin@mail.ru>
2019-02-19 22:07:02 +0300

    second hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,110 @@
-'use strict';
+"use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-  const cartWrapper = document.querySelector('.cart__wrapper');
-  const cart = document.querySelector('.cart');
-  const close = document.querySelector('.cart__close');
-  const confirm = document.querySelector('.cart__confirm');
-  const open = document.querySelector('#cart');
-  const goodsBtn = document.querySelectorAll('.goods__btn');
-  const products = document.querySelectorAll('.goods__item');
-  const badge = document.querySelector('.nav__badge');
-  const totalCost = document.querySelector('.cart__total>span');
-  const titles = document.querySelectorAll('.goods__title');
+window.addEventListener("DOMContentLoaded", () => {
+  const cartWrapper = document.querySelector(".cart__wrapper");
+  const cart = document.querySelector(".cart");
+  const close = document.querySelector(".cart__close");
+  const confirm = document.querySelector(".confirm");
+  const open = document.querySelector("#cart");
+  const goodsBtn = document.querySelectorAll(".goods__btn");
+  const products = document.querySelectorAll(".goods__item");
+  const badge = document.querySelector(".nav__badge");
+  const totalCost = document.querySelector(".cart__total>span");
+  const titles = document.querySelectorAll(".goods__title");
+  const empty = cartWrapper.querySelector(".empty");
 
   function openCart() {
-    cart.style.display = 'block';
-    document.body.style.overflow = 'hidden';
+    cart.style.display = "block";
+    document.body.style.overflow = "hidden";
   }
 
   function closeCart() {
-    cart.style.display = 'none';
-    document.body.style.overflow = '';
+    cart.style.display = "none";
+    document.body.style.overflow = "";
   }
 
-  open.addEventListener('click', openCart);
-  close.addEventListener('click', closeCart);
+  open.addEventListener("click", openCart);
+  close.addEventListener("click", closeCart);
 
-  goodsBtn.forEach(function openCart(btn, i) {
-    btn.addEventListener('click', () => {
+  goodsBtn.forEach(function(btn, i) {
+    btn.addEventListener("click", () => {
       let item = products[i].cloneNode(true);
-      let trigger = item.querySelector('button');
-      let removeBtn = document.createElement('div');
-      let empty = cartWrapper.querySelector('.empty');
+      let trigger = item.querySelector("button");
+      let removeBtn = document.createElement("div");
 
       trigger.remove();
-      removeBtn.classList.add('goods__item-remove');
-      removeBtn.innerHTML = '&times';
-      item.appendChild(removeBtn);
 
+      showConfirm();
+
+      calcGoods(1);
+
+      removeBtn.classList.add("goods__item-remove");
+      removeBtn.innerHTML = "&times";
+      item.appendChild(removeBtn);
       cartWrapper.appendChild(item);
-      if (empty) {
-        empty.remove();
-      }
+
+      calcTotal();
+      removeFromCart();
     });
   });
+
+  function sliceTitle() {
+    titles.forEach(function(item) {
+      if (item.textContent.length < 70) {
+        return;
+      } else {
+        item.textContent = `${item.textContent.slice(0, 71)}...`;
+      }
+    });
+  }
+  sliceTitle();
+
+  function showConfirm() {
+    confirm.style.display = "block";
+    let counter = 100;
+    const id = setInterval(frame, 10);
+
+    function frame() {
+      if (counter === 10) {
+        clearInterval(id);
+        confirm.style.display = "none";
+      } else {
+        counter--;
+        confirm.style.transform = `translateY(-${counter}px)`;
+        confirm.style.opacity = `.${counter}`;
+      }
+    }
+  }
+
+  function calcGoods(i) {
+    const items = cartWrapper.querySelectorAll(".goods__item");
+    badge.textContent = items.length + i;
+    if (items.length === 0) {
+      empty.style.display = "block";
+    } else {
+      empty.style.display = "none";
+    }
+  }
+
+  function calcTotal() {
+    const prices = document.querySelectorAll(
+      ".cart__wrapper>.goods__item>.goods__price>span"
+    );
+    let total = 0;
+    prices.forEach(function(item) {
+      total += +item.textContent;
+    });
+    totalCost.textContent = total;
+  }
+
+  function removeFromCart() {
+    const removeBtn = cartWrapper.querySelectorAll(".goods__item-remove");
+    removeBtn.forEach(function(btn) {
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+        calcGoods(0);
+        calcTotal();
+      });
+    });
+  }
 });
